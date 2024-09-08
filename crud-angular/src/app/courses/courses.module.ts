412f9c78799a5debfbff238b78aca693d5c1426b
anamsilva1981 <ana.msilva@hotmail.com>
2024-09-08 18:27:44 -0300

    [feature] Create Material Table to list courses

--- crud-angular/src/app/courses/courses.module.ts
+++ crud-angular/src/app/courses/courses.module.ts
@@ -4,14 +4,11 @@ import { CommonModule } from '@angular/common';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
 
+// Angular Material
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    CoursesComponent
-  ],
-  imports: [
-    CommonModule,
-    CoursesRoutingModule
-  ]
+  declarations: [CoursesComponent],
+  imports: [CommonModule, CoursesRoutingModule, MatTableModule],
 })
-export class CoursesModule { }
+export class CoursesModule {}
